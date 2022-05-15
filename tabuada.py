371411f020f508633bd70635d61fa59fcbb941ee
Samara Silva <samarasilva.dev@gmail.com>
2022-05-15 00:57:21 -0300

    tabuada utilizando while

---
+++ tabuada.py
@@ -0,0 +1,15 @@
+# tabuada utilizando while
+from traceback import print_tb
+
+i = 0
+
+numero = int(input("Digite o número desejado para visualizar a tabuada: "))
+
+while i < 10:
+    print(f'{numero} * {i + 1} = {numero * (i + 1)}')
+    numero + 1
+    i += 1
+
+
+
+
